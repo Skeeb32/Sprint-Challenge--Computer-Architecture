0f053de5ea1008dab5590d5a4c862485e0483445
Shaqib Habib <55864051+Skeeb32@users.noreply.github.com>
2020-05-22 09:02:35 -0700

    Add the CMP & Flag

--- ls8/cpu.py
+++ ls8/cpu.py
@@ -1,15 +1,4 @@
 import sys
-import os
-
-LDI = 0b10000010
-PRN = 0b01000111
-HLT = 0b00000001
-MUL = 0b10100010
-PUSH = 0b01000101
-POP = 0b01000110
-CALL = 0b01010000
-RET = 0b00010001
-ADD = 0b10100000
 
 class CPU:
     """Main CPU class."""
@@ -20,7 +9,19 @@ class CPU:
         self.pc = 0
         self.reg = [0] * 8
         self.ram = [0] * 256
-        self.SP = 0xf3
+        self.SP = 7
+        self.reg[self.SP] = 0xF4
+        self.flag = {}
+        self.LDI = 0b10000010
+        self.PRN = 0b01000111
+        self.HLT = 0b00000001
+        self.MUL = 0b10100010
+        self.PUSH = 0b01000101
+        self.POP = 0b01000110
+        self.CALL = 0b01010000
+        self.RET = 0b00010001
+        self.ADD = 0b10100000
+        self.CMP = 0b10100111
 
     def load(self, filename):
         """Load a program into memory."""
@@ -78,10 +79,23 @@ class CPU:
 
     def alu(self, op, reg_a, reg_b):
         """ALU operations."""
-        if op == "ADD":
+        if op == self.ADD:
             self.reg[reg_a] += self.reg[reg_b]
-        elif op == MUL:
+        elif op == self.MUL:
             self.reg[reg_a] *= self.reg[reg_b]
+        elif op == self.CMP:
+            if self.reg[reg_a] == self.reg[reg_b]:
+                self.flag['E'] = 1
+            else:
+                self.flag['E'] = 0
+            if self.reg[reg_a] < self.reg[reg_b]:
+                self.flag['L'] = 1
+            else:
+                self.flag['L'] = 0
+            if self.reg[reg_a] > self.reg[reg_b]:
+                self.flag['G'] = 1
+            else:
+                self.flag['G'] = 0
         #elif op == "SUB": etc
         else:
             raise Exception("Unsupported ALU operation")
@@ -107,46 +121,51 @@ class CPU:
 
     def run(self):
         """Run the CPU."""
-        while True:
-            IR = self.ram_read(self.pc)
-            opcode = self.ram[self.pc]
+        halted = False
+        while not halted:
+            # IR = Instruction Register, contains a copy of the currently executing instruction
+            IR = self.ram[self.pc]
             operand_a = self.ram_read(self.pc + 1)
             operand_b = self.ram_read(self.pc + 2)
             # print(IR, self.pc, operand_a, operand_b)
-            if opcode == LDI:
+            if IR == self.LDI:
                 self.reg[operand_a] = operand_b
                 # print(self.pc, self.reg, self.ram)
                 self.pc += 3
-            elif opcode == PRN:
-                print(self.reg[operand_a])
+            elif IR == self.PRN:
+                value = int(self.reg[operand_a])
+                print(f'{value}')
                 self.pc += 2
-            elif opcode == MUL:
-                self.alu(opcode, operand_a, operand_b)
+            elif IR == self.MUL:
+                self.alu(IR, operand_a, operand_b)
                 self.pc += 3
-            elif opcode == PUSH:
+            elif IR == self.PUSH:
                 operand_a = self.ram_read(self.pc + 1) 
                 self.ram_write(self.SP, self.reg[operand_a])
                 self.pc += 2
                 self.SP -= 1
-            elif opcode == POP:
+            elif IR == self.POP:
                 operand_a = self.ram_read(self.pc + 1)
                 self.reg[operand_a] = self.ram_read(self.SP+1)
                 self.SP += 1
                 self.pc += 2
-            elif opcode == CALL:
+            elif IR == self.CALL:
                 # print(operand_a)
                 operand_a = self.ram_read(self.pc + 1)
                 self.SP -= 1
                 self.ram_write(self.SP, self.pc + 2)
                 self.pc = self.reg[operand_a]
                 # print(self.pc)
-            elif opcode == RET:
+            elif IR == self.RET:
                 self.pc = self.ram_read(self.SP)
                 self.SP += 1
-            elif opcode == ADD:
+            elif IR == self.ADD:
                 self.pc +=3
-                self.alu("ADD", operand_a, operand_b)
-            elif opcode == HLT:
+                self.alu(self.ADD, operand_a, operand_b)
+            elif IR == self.CMP:
+                self.alu(self.CMP, operand_a, operand_b)
+                self.pc += 3
+            elif opcode == self.HLT:
                 sys.exit(0)
             else:
                 print(f"Did not work")
